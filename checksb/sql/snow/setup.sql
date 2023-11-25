5feda48480ab94fd9b9984604bcb78a66e265e51
BohuTANG <overred.shuttler@gmail.com>
2023-11-25 15:04:48 +0800

    Update setup.sql

--- checksb/sql/snow/setup.sql
+++ checksb/sql/snow/setup.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS assets;
 CREATE TABLE assets (
                         user_id INT NOT NULL,
                         asset_type VARCHAR NOT NULL,
@@ -6,6 +7,7 @@ CREATE TABLE assets (
                         last_updated DATE NOT NULL
 );
 
+DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
                         order_id INT NOT NULL,
                         user_id INT NOT NULL,
@@ -18,6 +20,7 @@ CREATE TABLE orders (
                         updated_at DATE NOT NULL
 );
 
+DROP TABLE IF EXISTS transactions;
 CREATE TABLE transactions (
                               transaction_id INT NOT NULL,
                               user_id INT NOT NULL,
@@ -32,31 +35,31 @@ CREATE OR REPLACE FILE FORMAT parquet_format TYPE = 'parquet';
 COPY INTO assets
     FROM (SELECT
     $1:user_id::INT,
-    $2:asset_type::VARCHAR,
-    $3:quantity::DECIMAL(18, 8),
-    $4:value::DECIMAL(18, 8),
-    $5:last_updated::DATE
+    $1:asset_type::VARCHAR,
+    $1:quantity::DECIMAL(18, 8),
+    $1:value::DECIMAL(18, 8),
+    $1:last_updated::DATE
     FROM @snow/assets (file_format => 'parquet_format'));
 
 COPY INTO orders
     FROM (SELECT
     $1:order_id::INT,
-    $2:user_id::INT,
-    $3:order_type::VARCHAR,
-    $4:asset_type::VARCHAR,
-    $5:quantity::DECIMAL(18, 8),
-    $6:price::DECIMAL(18, 8),
-    $7:status::VARCHAR,
-    $8:created_at::DATE,
-    $9:updated_at::DATE
+    $1:user_id::INT,
+    $1:order_type::VARCHAR,
+    $1:asset_type::VARCHAR,
+    $1:quantity::DECIMAL(18, 8),
+    $1:price::DECIMAL(18, 8),
+    $1:status::VARCHAR,
+    $1:created_at::DATE,
+    $1:updated_at::DATE
     FROM @snow/orders (file_format => 'parquet_format'));
 
 COPY INTO transactions
     FROM (SELECT
     $1:transaction_id::INT,
-    $2:user_id::INT,
-    $3:transaction_type::VARCHAR,
-    $4:asset_type::VARCHAR,
-    $5:quantity::DECIMAL(18, 8),
-    $6:transaction_time::DATE
+    $1:user_id::INT,
+    $1:transaction_type::VARCHAR,
+    $1:asset_type::VARCHAR,
+    $1:quantity::DECIMAL(18, 8),
+    $1:transaction_time::DATE
     FROM @snow/transactions (file_format => 'parquet_format'));
