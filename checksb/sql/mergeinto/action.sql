158fa3c5b71d259f4b8b736d1a28e85f8610449a
BohuTANG <overred.shuttler@gmail.com>
2023-11-28 16:09:07 +0800

    checksb: rename the number of the mergeinto

--- checksb/sql/mergeinto/action.sql
+++ checksb/sql/mergeinto/action.sql
@@ -1,33 +1,33 @@
--- Query M1: Update 'pending' orders to 'completed'
+-- Query MA1: Update 'pending' orders to 'completed'
 MERGE INTO orders USING (SELECT * FROM orders WHERE status = 'pending') AS pending_orders
     ON orders.order_id = pending_orders.order_id
     WHEN MATCHED THEN UPDATE SET orders.status = 'completed';
 
--- Query M2: Update quantities based on user and asset type sums
+-- Query MA2: Update quantities based on user and asset type sums
 MERGE INTO orders USING (
     SELECT user_id, asset_type, SUM(quantity) AS total_quantity FROM orders GROUP BY user_id, asset_type
 ) AS agg_orders
     ON orders.user_id = agg_orders.user_id AND orders.asset_type = agg_orders.asset_type
     WHEN MATCHED THEN UPDATE SET orders.quantity = agg_orders.total_quantity;
 
--- Query M3: Archive orders created before 2022-01-01
+-- Query MA3: Archive orders created before 2022-01-01
 MERGE INTO orders USING (SELECT * FROM orders WHERE created_at < '2022-01-01') AS old_orders
     ON orders.order_id = old_orders.order_id
     WHEN MATCHED THEN UPDATE SET orders.status = 'archived';
 
--- Query M4: Randomly swap 'buy' and 'sell' order types
+-- Query MA4: Randomly swap 'buy' and 'sell' order types
 MERGE INTO orders USING (
     SELECT order_id, user_id, CASE WHEN order_type = 'buy' THEN 'sell' ELSE 'buy' END AS order_type, asset_type, quantity, price, status, created_at, updated_at FROM orders
 ) AS swapped_orders
     ON orders.order_id = swapped_orders.order_id
     WHEN MATCHED THEN UPDATE SET orders.order_type = swapped_orders.order_type;
 
--- Query M5: Increase each order's quantity by 10
+-- Query MA5: Increase each order's quantity by 10
 MERGE INTO orders USING (SELECT * FROM orders) AS all_orders
     ON orders.order_id = all_orders.order_id
     WHEN MATCHED THEN UPDATE SET orders.quantity = orders.quantity + 10;
 
--- Query M6: Duplicate orders with new IDs
+-- Query MA6: Duplicate orders with new IDs
 MERGE INTO orders USING (
     SELECT order_id + 500000 AS new_order_id, user_id, order_type, asset_type, quantity, price, status, created_at, updated_at FROM orders
 ) AS duplicate_orders
@@ -35,26 +35,26 @@ MERGE INTO orders USING (
     WHEN NOT MATCHED THEN INSERT (order_id, user_id, order_type, asset_type, quantity, price, status, created_at, updated_at)
         VALUES (duplicate_orders.new_order_id, duplicate_orders.user_id, duplicate_orders.order_type, duplicate_orders.asset_type, duplicate_orders.quantity, duplicate_orders.price, duplicate_orders.status, duplicate_orders.created_at, duplicate_orders.updated_at);
 
--- Query M7: Update orders with joined average quantities
+-- Query MA7: Update orders with joined average quantities
 MERGE INTO orders USING (
     SELECT o.order_id, o.user_id, o.order_type, o.asset_type, o.quantity + a.avg_quantity AS new_quantity, o.price, o.status, o.created_at, o.updated_at FROM orders o INNER JOIN (SELECT user_id, asset_type, AVG(quantity) AS avg_quantity FROM orders GROUP BY user_id, asset_type) a ON o.user_id = a.user_id AND o.asset_type = a.asset_type
 ) AS joined_data
     ON orders.order_id = joined_data.order_id
     WHEN MATCHED THEN UPDATE SET orders.quantity = joined_data.new_quantity;
 
--- Query M8: Update status for orders between 2021-01-01 and 2021-06-30
+-- Query MA8: Update status for orders between 2021-01-01 and 2021-06-30
 MERGE INTO orders USING (SELECT * FROM orders WHERE created_at BETWEEN '2021-01-01' AND '2021-06-30') AS date_filtered
     ON orders.order_id = date_filtered.order_id
     WHEN MATCHED THEN UPDATE SET orders.status = CASE WHEN orders.status = 'pending' THEN 'expired' ELSE orders.status END;
 
--- Query M9: Update status based on quantity vs average
+-- Query MA9: Update status based on quantity vs average
 MERGE INTO orders USING (
     SELECT o.order_id, o.user_id, o.order_type, o.asset_type, o.quantity, o.price, o.status, o.created_at, o.updated_at, CASE WHEN o.quantity < sub.avg_quantity THEN 'below_avg' WHEN o.quantity > sub.avg_quantity THEN 'above_avg' ELSE 'avg' END AS quantity_status FROM orders o INNER JOIN (SELECT user_id, asset_type, AVG(quantity) AS avg_quantity FROM orders GROUP BY user_id, asset_type) sub ON o.user_id = sub.user_id AND o.asset_type = sub.asset_type
 ) AS complex_data
     ON orders.order_id = complex_data.order_id
     WHEN MATCHED THEN UPDATE SET orders.status = complex_data.quantity_status;
 
--- Query M10: Update price based on average asset value
+-- Query MA10: Update price based on average asset value
 MERGE INTO orders USING (
     SELECT a.user_id, a.asset_type, AVG(a.value) as avg_value FROM assets a GROUP BY a.user_id, a.asset_type
 ) AS asset_data
--- checksb/sql/mergeinto/check.sql
+++ checksb/sql/mergeinto/check.sql
@@ -1,125 +1,113 @@
--- 1. Asset Types Distribution
+-- Query MC1: Asset Types Distribution
 SELECT asset_type, COUNT(*) AS count
 FROM assets
 GROUP BY asset_type
 ORDER BY count DESC, asset_type
     LIMIT 13;
 
--- 2. Aggregated Quantity and Value Statistics
--- (No LIMIT needed as it returns a single row)
-SELECT
-    SUM(quantity) AS total_quantity,
-    AVG(quantity) AS average_quantity,
-    SUM(value) AS total_value,
-    AVG(value) AS average_value
+-- Query MC2: Aggregated Quantity and Value Statistics
+SELECT SUM(quantity) AS total_quantity,
+       AVG(quantity) AS average_quantity,
+       SUM(value) AS total_value,
+       AVG(value) AS average_value
 FROM assets;
 
--- 3. Assets Counts by User
+-- Query MC3: Assets Counts by User
 SELECT user_id, COUNT(*) AS count
 FROM assets
 GROUP BY user_id
 ORDER BY count DESC, user_id
     LIMIT 13;
 
--- 4. Date Range Analysis of Last Update
-SELECT
-    CASE
-        WHEN last_updated < '2022-01-01' THEN 'Before 2022'
-        ELSE 'After 2021-12-31'
-        END AS date_range,
-    COUNT(*) AS count
+-- Query MC4: Date Range Analysis of Last Update
+SELECT CASE
+           WHEN last_updated < '2022-01-01' THEN 'Before 2022'
+           ELSE 'After 2021-12-31'
+           END AS date_range,
+       COUNT(*) AS count
 FROM assets
 GROUP BY date_range
 ORDER BY date_range
     LIMIT 13;
 
--- 5. General Status Distribution
+-- Query MC5: General Status Distribution
 SELECT status, COUNT(*) AS count
 FROM orders
 GROUP BY status
 ORDER BY count DESC, status
     LIMIT 13;
 
--- 6. General Quantity Statistics
--- (No LIMIT needed as it returns a single row)
-SELECT
-    SUM(quantity) AS total_quantity,
-    AVG(quantity) AS average_quantity,
-    MIN(quantity) AS min_quantity,
-    MAX(quantity) AS max_quantity
+-- Query MC6: General Quantity Statistics
+SELECT SUM(quantity) AS total_quantity,
+       AVG(quantity) AS average_quantity,
+       MIN(quantity) AS min_quantity,
+       MAX(quantity) AS max_quantity
 FROM orders;
 
--- 7. New Orders vs Existing Orders Count
-SELECT
-    CASE
-        WHEN order_id > 500000 THEN 'New Order'
-        ELSE 'Existing Order'
-        END AS order_category,
-    COUNT(*) AS count
+-- Query MC7: New Orders vs Existing Orders Count
+SELECT CASE
+           WHEN order_id > 500000 THEN 'New Order'
+           ELSE 'Existing Order'
+           END AS order_category,
+       COUNT(*) AS count
 FROM orders
 GROUP BY order_category
 ORDER BY count DESC
     LIMIT 13;
 
--- 8. Order Type Distribution
+-- Query MC8: Order Type Distribution
 SELECT order_type, COUNT(*) AS count
 FROM orders
 GROUP BY order_type
 ORDER BY count DESC, order_type
     LIMIT 13;
 
--- 9. Date Range Analysis
-SELECT
-    CASE
-        WHEN created_at < '2022-01-01' THEN 'Before 2022'
-        WHEN created_at BETWEEN '2021-01-01' AND '2021-06-30' THEN 'First Half 2021'
-        ELSE 'After 2021-06-30'
-        END AS date_range,
-    COUNT(*) AS count
+-- Query MC9: Date Range Analysis
+SELECT CASE
+           WHEN created_at < '2022-01-01' THEN 'Before 2022'
+           WHEN created_at BETWEEN '2021-01-01' AND '2021-06-30' THEN 'First Half 2021'
+           ELSE 'After 2021-06-30'
+           END AS date_range,
+       COUNT(*) AS count
 FROM orders
 GROUP BY date_range
 ORDER BY date_range
     LIMIT 13;
 
--- 10. Price Analysis After Adjustments
--- (No LIMIT needed as it returns a single row)
-SELECT
-    SUM(price) AS total_price,
-    AVG(price) AS average_price,
-    MIN(price) AS min_price,
-    MAX(price) AS max_price
+-- Query MC10: Price Analysis After Adjustments
+SELECT SUM(price) AS total_price,
+       AVG(price) AS average_price,
+       MIN(price) AS min_price,
+       MAX(price) AS max_price
 FROM orders;
 
--- 11. Transaction Types Distribution
+-- Query MC11: Transaction Types Distribution
 SELECT transaction_type, COUNT(*) AS count
 FROM transactions
 GROUP BY transaction_type
 ORDER BY count DESC, transaction_type
     LIMIT 13;
 
--- 12. Aggregated Quantity Statistics
--- (No LIMIT needed as it returns a single row)
-SELECT
-    SUM(quantity) AS total_quantity,
-    AVG(quantity) AS average_quantity,
-    MIN(quantity) AS min_quantity,
-    MAX(quantity) AS max_quantity
+-- Query MC12: Aggregated Quantity Statistics
+SELECT SUM(quantity) AS total_quantity,
+       AVG(quantity) AS average_quantity,
+       MIN(quantity) AS min_quantity,
+       MAX(quantity) AS max_quantity
 FROM transactions;
 
--- 13. Transaction Counts by User and Asset Type
+-- Query MC13: Transaction Counts by User and Asset Type
 SELECT user_id, asset_type, COUNT(*) AS count
 FROM transactions
 GROUP BY user_id, asset_type
 ORDER BY count DESC, user_id, asset_type
     LIMIT 13;
 
--- 14. Date Range Analysis of Transactions
-SELECT
-    CASE
-        WHEN transaction_time < '2022-01-01' THEN 'Before 2022'
-        ELSE 'After 2021-12-31'
-        END AS date_range,
-    COUNT(*) AS count
+-- Query MC14: Date Range Analysis of Transactions
+SELECT CASE
+           WHEN transaction_time < '2022-01-01' THEN 'Before 2022'
+           ELSE 'After 2021-12-31'
+           END AS date_range,
+       COUNT(*) AS count
 FROM transactions
 GROUP BY date_range
 ORDER BY date_range
