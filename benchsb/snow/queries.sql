3b29665cadce05873becacf5ae900deeaca56792
xudong963 <wxd963996380@gmail.com>
2024-01-11 23:11:04 +0800

    chore: fix snowflake q15

--- benchsb/snow/queries.sql
+++ benchsb/snow/queries.sql
@@ -66,7 +66,7 @@ order by
 	s_name,
 	p_partkey LIMIT 100;
 
-    
+
 -- TPC-H 3
 select
 	l_orderkey,
@@ -399,25 +399,9 @@ where
 	AND l_shipdate < DATEADD(month, 1, '1995-09-01');
 
 -- TPC-H 15
-select
-	s_suppkey,
-	s_name,
-	s_address,
-	s_phone,
-	total_revenue
-from
-	supplier,
-	revenue0
-where
-	s_suppkey = supplier_no
-	and total_revenue = (
-		select
-			max(total_revenue)
-		from
-			revenue0
-	)
-order by
-	s_suppkey;
+with revenue as ( select l_suppkey as supplier_no, sum(l_extendedprice * (1 - l_discount)) as total_revenue from lineitem where l_shipdate >= to_date ('1996-01-01') and l_shipdate < to_date ('1996-04-01') group by l_suppkey)
+
+select s_suppkey, s_name, s_address, s_phone, total_revenue from supplier, revenue where s_suppkey = supplier_no and total_revenue = ( select max(total_revenue) from revenue ) order by s_suppkey;
 
 
 -- TPC-H 16
